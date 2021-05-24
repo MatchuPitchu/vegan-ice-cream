25e51741eddd2e1008b5f41d4623688f80c89b19
Matchu Pitchu <77695211+MatchuPitchu@users.noreply.github.com>
2021-05-24 19:07:29 +0200

    Improve search map with highlighting

--- src/components/Search.js
+++ src/components/Search.js
@@ -16,34 +16,28 @@ const Search = () => {
   const [ popoverShow, setPopoverShow ] = useState({ show: false, event: undefined });
   const [ searchWords, setSearchWords ] = useState([]);
 
-  const onSubmit = (e) => {
-    e.preventDefault();
-    const res = locations.filter(loc => loc.name.toLowerCase().includes(searchText.toLowerCase()) || loc.address.city.toLowerCase().includes(searchText.toLowerCase()) );
-    const result = res.slice(0, 10);
-    setPredictions(result);
-  }
+  const onSubmit = e => e.preventDefault();
 
   const forAutocompleteChange = async value => {
     if(value.length >= 3 && locations) {
-
+      // make array from input string -> each item is created after one space " "
+      const searchQuery = value.split(' ').filter(word => word);
       const res = await locations.filter(loc => {
-        const found = searchWords.map(word => {
+        const found = searchQuery.map(word => {
           // return if exists just a space or a space and then nothing
           if (word === " " || word === "") return;
           // explanation: http://stackoverflow.com/a/18622606/1147859
           const reg = "(" + word + ")(?![^<]*>|[^<>]*</)";
           // i means case-insensitive mode
           const regex = new RegExp(reg, "i");
-          console.log('regex test:', loc.name, regex.test(loc.name + loc.address.city) )
-          return regex.test(loc.name + loc.address.city)
+          console.log('regex test:', loc.name, regex.test(`${loc.name} ${loc.address.street} ${loc.address.number} ${loc.address.city}`) )
+          return regex.test(`${loc.name} ${loc.address.street} ${loc.address.number} ${loc.address.city}`)
         });
-        console.log("found", found)
+        // found is array with as many items as there are search words
+        // if every item is true, than this location is returned
         if(found.every(v => v === true)) return loc;
       });
-      
-      console.log('res', res)
-      
-      const result = res.slice(0, 10);
+      const result = res.slice(0, 4);
       setPredictions(result);
     }
     if(!value) {
@@ -51,135 +45,6 @@ const Search = () => {
       setSearchSelected(null)
     }
   };
-
-  // const forAutocompleteChange = async value => {
-  //   if(value.length >= 3 && locations) {
-
-  //     // HAVE TO COMBINE https://benfrain.com/building-search-results-and-highlighting-matches-with-regex/
-  //     // AND THIS https://github.com/bvaughn/react-highlight-words
-
-  //     // const highlightMatches = (string, query) => {
-  //     //   // the completed string will be itself if already set, otherwise, the string that was passed in
-  //     //   var completedString = completedString || string;
-  //     //   query.forEach(word => {
-  //     //     const reg = "(" + word + ")(?![^<]*>|[^<>]*</)";
-  //     //     const regex = new RegExp(reg, "i");
-  //     //     // return if regex does not match string
-  //     //     if(!string.match(regex)) return;
-  //     //     // set highlighting
-  //     //     const matchStart = string.match(regex).index;
-  //     //     const matchEnd = matchStart + string.match(regex)[0].toString().length;
-  //     //     const textFound = string.substring(matchStart, matchEnd);
-
-  //     //     completedString = completedString.replace(regex, `<span class="highlight">${textFound}</span>`)
-  //     //   })
-  //     //   console.log("completedString:", completedString)
-  //     //   setPredictions(prev => [...prev, completedString]);
-  //     // }
-
-
-  //     const buildPredictions = (query, loc) => {
-  //       // make array from input string -> each item is created after one space " "
-  //       query = query.split(" ");
-        
-  //       console.log('query.split:', query)
-
-  //       query.forEach(word => {
-  //         // return if exists just a space or a space and then nothing
-  //         if (word === " " || word === "") return;
-  //         // explanation: http://stackoverflow.com/a/18622606/1147859
-  //         const reg = "(" + word + ")(?![^<]*>|[^<>]*</)";
-  //         // i means case-insensitive mode
-  //         const regex = new RegExp(reg, "i");
-
-  //         // remove loc from predictions array if condition is given
-  //         if ( 
-  //           !loc.name.match(regex) &&
-  //           !loc.address.city.match(regex)
-  //           // !loc.address.street.match(regex) && 
-  //           // !loc.address.number.match(regex) && 
-  //           // !loc.address.zipcode.match(regex)
-  //         ) {
-  //           const index = predictions.findIndex(pre => pre.name === loc.name && pre.address.city === loc.address.city)
-  //           console.log('index:', index)
-  //           if(index !== -1) setPredictions(() => predictions.splice(index, 1));
-  //           return
-  //         };
-          
-  //         // add loc to predictions array if condition is given
-  //         if ( loc.name.match(regex) && loc.address.city.match(regex) ) {
-  //           const index = predictions.findIndex(pre => pre.name === loc.name && pre.address.city === loc.address.city)
-  //           console.log('index:', index)
-  //           if(index === -1) setPredictions(prev => [...prev, loc]);
-  //         }
-
-  //         // highlightMatches(loc.name, query);        
-  //       });
-  //     }
-
-  //     // for (let i = 0; i < locations.length; i++) {
-  //     //   buildPredictions(value, locations[i])
-  //     // }
-
-
-      
-  //     // const res = stringForSearchArr.filter(string => string.match(regex) );
-  //     // console.log(res);
-  //     // const result = res.slice(0, 10);
-  //     // setPredictions(result);
-
-  //     const buildPredict = (query, string) => {
-  //       // make array from input string -> each item is created after one space " "
-  //       query = query.split(" ");
-  //       console.log('query.split:', query)
-
-  //       query.forEach(word => {
-  //         // return if exists just a space or a space and then nothing
-  //         if (word === " " || word === "") return;
-  //         // explanation: http://stackoverflow.com/a/18622606/1147859
-  //         const reg = "(" + word + ")(?![^<]*>|[^<>]*</)";
-  //         // i means case-insensitive mode
-  //         const regex = new RegExp(reg, "i");
-          
-  //         if(!string.match(regex)) {
-  //           const index = result.findIndex(item => item === string)
-  //           console.log('indexREMOVE:', index)
-  //           if(index !== -1) setResult(() => result.splice(index, 1));
-  //           return
-  //         }
-  //         console.log('result', string.match(regex))
-
-  //         const index = result.findIndex(item => item === string);
-  //         console.log('indexADD', index)
-  //         if(index === -1) setResult(() => [...result, string])   
-
-  //       //   const res = stringForSearchArr.filter(string => {
-  //       //     if(!string.match(regex)) return
-  //       //     string.match(regex)
-  //       //   })
-
-  //       // setResult(() => [result, stringForSearchArr.filter(string => string.match(regex))] );
-
-  //       // console.log('res', result);
-
-  //       })
-  //     };
-
-  //     for (let i = 0; i < stringForSearchArr.length; i++) {
-  //       buildPredict(value, stringForSearchArr[i])
-  //     }
-
-  //     // const res = await locations.filter(loc => loc.name.toLowerCase().includes(value.toLowerCase()) || loc.address.city.toLowerCase().includes(value.toLowerCase()) );
-  //     // const result = res.slice(0, 10);
-  //     // setPredictions(result);
-  //   }
-  //   if(!value) {
-  //     setPredictions([])
-  //     setSearchSelected(null)
-  //   }
-  // };
-
-  console.log("predictions", predictions);
 
   const initMarker = (loc) => {
     setSearchSelected(loc); 
