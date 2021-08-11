783bb5596b0fb28d855228d67c70944f01c35b5a
MatchuPitchu <michael.flohr@posteo.de>
2021-08-11 13:03:15 +0200

    Add ServiceWorker functionality update notification + compress images

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,6 @@
-import { useContext } from 'react';
+import { useContext, useEffect } from 'react';
 import { Context } from "./context/Context";
-import { Redirect, Route } from 'react-router-dom';
-import { IonReactRouter } from '@ionic/react-router';
+import { Redirect, Route, useHistory } from 'react-router-dom';
 import {
   IonApp,
   IonIcon,
@@ -25,7 +24,6 @@ import Register from './components/Auth/Register';
 import ActivateUser from './components/Auth/ActivateUser';
 import Logout from './components/Auth/Logout';
 import IosDescription from './components/IosDescription';
-
 
 /* Core CSS required for Ionic components to work properly */
 import '@ionic/react/css/core.css';
@@ -54,54 +52,63 @@ import Datenschutz from './pages/Datenschutz';
 const App: React.FC = () => {
   const { user } = useContext(Context);
 
+  // to display notification if update available (see ServiceWorkerWrapper.js) 
+  // without reload of page
+  const history = useHistory();
+  useEffect(() => {
+    history.listen((location, action) => {
+      // check for sw updates on page change
+      navigator.serviceWorker
+        .getRegistrations()
+        .then((regs) => regs.forEach((reg) => reg.update()));
+    });
+  }, []);
+
   return (
     <IonApp>
-      <IonReactRouter>
-        
-        <HeaderApp />
-      
-        <IonContent>
-          <IonTabs>
-            <IonRouterOutlet>
-              <Redirect from="/" to="/home" exact />
-              <Route path="/home" component={Home} exact />
-              <Route path="/entdecken" component={Entdecken} exact />
-              <Route path="/bewerten" component={Bewerten} exact />
-              <Route path="/preis" component={Preis} exact />
-              <Route path="/favoriten" component={Favoriten} exact />
-              <Route path="/login" component={Login} exact />
-              <Route path="/register" component={Register} exact />
-              <Route path="/logout" component={Logout} exact/>
-              <Route path="/auth/activate/user/:id" component={ActivateUser} exact/>
-              <Route path="/auth/reset-password" component={ResetPassword} exact/>
-              <Route path="/auth/reset-password/user/:id" component={SetNewPassword} exact/>
-              <Route path="/datenschutz" component={Datenschutz} exact/>
-              <Route path="/ios" component={IosDescription} exact/>
-            </IonRouterOutlet>
-            <IonTabBar slot="bottom">
-              <IonTabButton tab="home" href="/home">
-                <IonIcon icon={home} />
-                <IonLabel className="labelTabs">Home</IonLabel>
-              </IonTabButton>
-              <IonTabButton tab="entdecken" href="/entdecken">
-                <IonIcon icon={disc} />
-                <IonLabel className="labelTabs">Entdecken</IonLabel>
-              </IonTabButton>
-              <IonTabButton tab="bewerten" href="/bewerten">
-                <IonIcon icon={iceCream} />
-                <IonLabel className="labelTabs">Bewerten</IonLabel>
-              </IonTabButton>
-              <IonTabButton tab="favoriten" href="/favoriten">
-                <IonIcon icon={bookmarks} />
-                <IonLabel className="labelTabs">Favoriten</IonLabel>
-                {user && (
-                  <IonBadge color="secondary">{user.favorite_locations.length || 0}</IonBadge>
-                )}
-              </IonTabButton>
-            </IonTabBar>
-          </IonTabs>
-        </IonContent>
-      </IonReactRouter>
+      <HeaderApp />
+    
+      <IonContent>
+        <IonTabs>
+          <IonRouterOutlet>
+            <Redirect from="/" to="/home" exact />
+            <Route path="/home" component={Home} exact />
+            <Route path="/entdecken" component={Entdecken} exact />
+            <Route path="/bewerten" component={Bewerten} exact />
+            <Route path="/preis" component={Preis} exact />
+            <Route path="/favoriten" component={Favoriten} exact />
+            <Route path="/login" component={Login} exact />
+            <Route path="/register" component={Register} exact />
+            <Route path="/logout" component={Logout} exact/>
+            <Route path="/auth/activate/user/:id" component={ActivateUser} exact/>
+            <Route path="/auth/reset-password" component={ResetPassword} exact/>
+            <Route path="/auth/reset-password/user/:id" component={SetNewPassword} exact/>
+            <Route path="/datenschutz" component={Datenschutz} exact/>
+            <Route path="/ios" component={IosDescription} exact/>
+          </IonRouterOutlet>
+          <IonTabBar slot="bottom">
+            <IonTabButton tab="home" href="/home">
+              <IonIcon icon={home} />
+              <IonLabel className="labelTabs">Home</IonLabel>
+            </IonTabButton>
+            <IonTabButton tab="entdecken" href="/entdecken">
+              <IonIcon icon={disc} />
+              <IonLabel className="labelTabs">Entdecken</IonLabel>
+            </IonTabButton>
+            <IonTabButton tab="bewerten" href="/bewerten">
+              <IonIcon icon={iceCream} />
+              <IonLabel className="labelTabs">Bewerten</IonLabel>
+            </IonTabButton>
+            <IonTabButton tab="favoriten" href="/favoriten">
+              <IonIcon icon={bookmarks} />
+              <IonLabel className="labelTabs">Favoriten</IonLabel>
+              {user && (
+                <IonBadge color="secondary">{user.favorite_locations.length || 0}</IonBadge>
+              )}
+            </IonTabButton>
+          </IonTabBar>
+        </IonTabs>
+      </IonContent>
     </IonApp>
   )
 }
--- src/index.tsx
+++ src/index.tsx
@@ -2,18 +2,23 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import Context from './context/Context';
 import App from './App';
-import * as serviceWorkerRegistration from './serviceWorkerRegistration';
+import ServiceWorkerWrapper from './components/ServiceWorkerWrapper';
+import { IonReactRouter } from '@ionic/react-router';
 
 ReactDOM.render(
   <React.StrictMode>
     <Context>
-      <App />
+      <IonReactRouter>
+        <App />
+        <ServiceWorkerWrapper />
+      </IonReactRouter>
     </Context>
   </React.StrictMode>,
   document.getElementById('root')
 );
 
+// => moved to ServiceWorker component like described here: https://felixgerschau.com/create-a-pwa-update-notification-with-create-react-app/
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
 // Learn more about service workers: https://cra.link/PWA
-serviceWorkerRegistration.register();
+// serviceWorker.register();
