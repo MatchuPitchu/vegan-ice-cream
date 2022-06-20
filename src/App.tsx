f62c1833a98b98b2ebf95e3ccf6f705b805bca21
MatchuPitchu <michael.flohr@posteo.de>
2022-06-20 23:58:43 +0200

    add files

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,8 @@
-import { useEffect } from 'react';
 import { Redirect, Route } from 'react-router-dom';
 // Redux Store
-import store from './store';
 import { useAppSelector } from './store/hooks';
 import { useGetAdditionalInfosFromUserQuery } from './store/api/user-api-slice';
-import { authApi } from './store/api/auth-api-slice';
-import { locationsApi, useGetAllCitiesWithLocationsQuery } from './store/api/locations-api-slice';
+import { useGetAllCitiesWithLocationsQuery } from './store/api/locations-api-slice';
 import { skipToken } from '@reduxjs/toolkit/dist/query';
 import {
   IonApp,
@@ -59,12 +56,6 @@ import './App.css';
 
 const App: React.FC = () => {
   const { user } = useAppSelector((state) => state.user);
-
-  useEffect(() => {
-    // trigger verifyUserSession and getLocations when App is started
-    store.dispatch(authApi.endpoints.verifyUserSession.initiate());
-    store.dispatch(locationsApi.endpoints.getLocations.initiate());
-  }, []);
 
   // GET Request: additional user informationen
   const {
