6db338253c80a8f0147b41eb008d7af0694aec7b
MatchuPitchu <michael.flohr@posteo.de>
2022-07-19 16:59:29 +0200

    style: adjust component favorite list item

--- src/components/Card/CardRatingsAndButtons.tsx
+++ src/components/Card/CardRatingsAndButtons.tsx
@@ -1,9 +1,8 @@
-import { VFC } from 'react';
+import { useContext, VFC } from 'react';
 import { useAppDispatch } from '../../store/hooks';
 import { locationsActions } from '../../store/locationsSlice';
-import { IonButton, IonIcon } from '@ionic/react';
-import { informationCircleOutline, starHalfOutline } from 'ionicons/icons';
-import Ratings from '../Ratings';
+import { IonIcon, NavContext } from '@ionic/react';
+import { informationCircleOutline, mapOutline, starHalfOutline } from 'ionicons/icons';
 import { showActions } from '../../store/showSlice';
 
 interface Props {
@@ -12,59 +11,64 @@ interface Props {
   locationId: string;
 }
 
-const CardRatingsAndButtons: VFC<Props> = ({ ratingVegan, ratingQuality, locationId }) => {
+const CardButtons: VFC<Props> = ({ ratingVegan, ratingQuality, locationId }) => {
+  const { navigate } = useContext(NavContext);
+
   const dispatch = useAppDispatch();
 
-  const selectLocation = () => {
-    dispatch(locationsActions.setSelectedLocation(locationId));
-  };
+  const selectLocation = () => dispatch(locationsActions.setSelectedLocation(locationId));
+
+  const hasRatings = ratingVegan && ratingQuality;
 
   return (
-    <div className='px-3 py-1'>
-      {ratingVegan && ratingQuality && (
-        <>
-          <Ratings rating_vegan_offer={ratingVegan} rating_quality={ratingQuality} showNum={true} />
-          <div className='button-group'>
-            <IonButton
-              className='button--check button--check-low-height'
-              fill='clear'
-              onClick={() => {
-                selectLocation();
-                dispatch(showActions.setShowLocationInfoModal(true));
-              }}
-            >
-              <IonIcon slot='end' size='small' icon={informationCircleOutline} />
-              Mehr Infos
-            </IonButton>
-            <IonButton
-              className='button--check button--check-low-height'
-              fill='clear'
-              onClick={() => selectLocation()}
-              routerLink='/bewerten'
-              routerDirection='forward'
-            >
-              <IonIcon slot='end' size='small' icon={starHalfOutline} />
-              Bewerten
-            </IonButton>
-          </div>
-        </>
+    <>
+      {hasRatings && (
+        <div className='button-group button-group--locations-list'>
+          <button
+            className='button--locations-list'
+            onClick={() => {
+              selectLocation();
+              dispatch(showActions.setShowLocationInfoModal(true));
+            }}
+          >
+            <IonIcon aria-label='Info' size='small' icon={informationCircleOutline} />
+          </button>
+          <button
+            className='button--locations-list'
+            onClick={() => {
+              selectLocation();
+              navigate('/entdecken', 'forward');
+            }}
+          >
+            <IonIcon aria-label='auf Karte zeigen' size='small' icon={mapOutline} />
+          </button>
+          <button
+            className='button--locations-list'
+            onClick={() => {
+              selectLocation();
+              navigate('/bewerten', 'forward');
+            }}
+          >
+            <IonIcon aria-label='Bewerten' size='small' icon={starHalfOutline} />
+          </button>
+        </div>
       )}
 
-      {!ratingQuality && (
-        <IonButton
-          className='button--check button--check-low-height button--check-large'
-          fill='clear'
-          expand='block'
-          onClick={() => selectLocation()}
-          routerLink='/bewerten'
-          routerDirection='forward'
-        >
-          <IonIcon slot='end' size='small' icon={starHalfOutline} />
-          Erste Bewertung schreiben
-        </IonButton>
+      {!hasRatings && (
+        <div className='button-group button-group--locations-list'>
+          <button
+            className='button--locations-list button--check-large'
+            onClick={() => {
+              selectLocation();
+              navigate('/bewerten', 'forward');
+            }}
+          >
+            Erste Bewertung schreiben
+          </button>
+        </div>
       )}
-    </div>
+    </>
   );
 };
 
-export default CardRatingsAndButtons;
+export default CardButtons;
